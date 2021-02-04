a47341705dc95a103509be83c2fe7f94ad4cea25
Vitaliy Danilov <vitaliy931@gmail.com>
2021-02-05 01:18:01 +0200

    ParallelTransform

--- AsyncEx/ParallelTransform.cs
+++ AsyncEx/ParallelTransform.cs
@@ -12,19 +12,21 @@
     public interface IParallelEnumerator<out TInput>
     {
         TInput Item { get; }
-        IEnumerable<Aggregator<TResult>> Run<TIn, TOut, TResult>(IEnumerable<TIn> items, 
+
+        IEnumerable<IAggregator<TResult>> Sub<TIn, TOut, TResult>(
+            IEnumerable<TIn> items,
+            Func<IParallelEnumerator<TIn>, IEnumerable<IAggregator<TOut>>> aggregateFunc,
+            Func<TIn, IList<TOut>, TResult> resultSelector);
+
+        IEnumerable<IAggregator<TResult>> Run<TIn, TOut, TResult>(
+            IEnumerable<TIn> items, 
             Func<TIn, Task<TOut>> func, Func<TIn, TOut, TResult> resultSelector);
     }
 
-    public sealed class Aggregator<TOutput>
+    public interface IAggregator<TOutput>
     {
-        internal Func<Task<TOutput>> Func { get; }
-        internal TOutput Result { get; set; } = default!;
-
-        public Aggregator(Func<Task<TOutput>> func)
-        {
-            Func = func;
-        }
+        Task InvokeAsync();
+        TOutput Result { get; }
     }
 
     /// <summary>
@@ -32,6 +34,24 @@
     /// </summary>
     public sealed class ParallelTransform
     {
+        private sealed class Aggregator<TOutput, TState> : IAggregator<TOutput>
+        {
+            private readonly Func<TState, Task<TOutput>> _asyncFunc;
+            public TOutput Result { get; private set; } = default!;
+            private readonly TState _state;
+
+            internal Aggregator(Func<TState, Task<TOutput>> func, TState state)
+            {
+                _asyncFunc = func;
+                _state = state;
+            }
+
+            public async Task InvokeAsync()
+            {
+                Result = await _asyncFunc(_state).ConfigureAwait(false);
+            }
+        }
+
         private sealed class ParallelEnumerator<TInput> : IParallelEnumerator<TInput>
         {
             public TInput Item { get; private set; } = default!;
@@ -41,70 +61,100 @@
                 Item = item;
             }
 
-            public IEnumerable<Aggregator<TRes>> Run<TIn, TOut, TRes>(IEnumerable<TIn> items, Func<TIn, Task<TOut>> func, Func<TIn, TOut, TRes> resultSelector)
+            public IEnumerable<IAggregator<TRes>> Run<TIn, TOut, TRes>(
+                IEnumerable<TIn> subItems, 
+                Func<TIn, Task<TOut>> func, 
+                Func<TIn, TOut, TRes> resultSelector)
             {
-                return items.Select(input => new Aggregator<TRes>(async () =>
+                foreach (TIn subItem in subItems)
                 {
-                    var tOut = await func(input).ConfigureAwait(false); ;
-                    return resultSelector(input, tOut);
-                }));
+                    yield return CreateAggregator(static async t =>
+                    {
+                        TOut result = await t.func(t.subItem).ConfigureAwait(false);
+                        return t.resultSelector(t.subItem, result);
+                    }, 
+                    state: (func, subItem, resultSelector));
+                }
+            }
+
+            public IEnumerable<IAggregator<TRes>> Sub<TIn, TInherim, TRes>(
+                IEnumerable<TIn> subItems, 
+                Func<IParallelEnumerator<TIn>, IEnumerable<IAggregator<TInherim>>> aggregateFunc, 
+                Func<TIn, IList<TInherim>, TRes> resultSelector)
+            {
+                foreach (TIn subItem in subItems)
+                {
+                    var en = new ParallelEnumerator<TIn>();
+                    en.MoveNext(subItem);
+
+                    IAggregator<TInherim>[] aggs = aggregateFunc(en).ToArray();
+
+                    yield return CreateAggregator(static async t =>
+                    {
+                        List<TInherim> outputs = new(t.aggs.Length);
+                        for (int i = 0; i < t.aggs.Length; i++)
+                        {
+                            var ag = t.aggs[i];
+                            await ag.InvokeAsync().ConfigureAwait(false);
+                            outputs.Add(ag.Result);
+                        }
+                        return t.resultSelector(t.subItem, outputs);
+                    },
+                    state: (subItem, aggs, resultSelector));
+                }
             }
         }
 
-        public static async Task<IList<TResult>> Run<TInput, TOutput, TResult>(IEnumerable<TInput> items,
-            Func<IParallelEnumerator<TInput>, IEnumerable<Aggregator<TOutput>>> func,
+        public static async Task<IList<TResult>> Transform<TInput, TOutput, TResult>(IEnumerable<TInput> items,
+            Func<IParallelEnumerator<TInput>, IEnumerable<IAggregator<TOutput>>> func,
             Func<TInput, IList<TOutput>, TResult> resultSelector,
-            int maxDegreeOfParallelism)
+            int maxDegreeOfParallelism = -1)
         {
             List<TResult> list = new();
-            var en = Enumerate(items, func, resultSelector, maxDegreeOfParallelism);
+            IAsyncEnumerable<TResult> en = EnumerateAsync(items, func, resultSelector, maxDegreeOfParallelism);
 
-            await foreach (var item in en)
+            await foreach (var result in en.ConfigureAwait(false))
             {
-                list.Add(item);
+                list.Add(result);
             }
             return list;
         }
 
-        public static async IAsyncEnumerable<TResult> Enumerate<TInput, TOutput, TResult>(IEnumerable<TInput> items,
-            Func<IParallelEnumerator<TInput>, IEnumerable<Aggregator<TOutput>>> func,
+        public static async IAsyncEnumerable<TResult> EnumerateAsync<TInput, TOutput, TResult>(IEnumerable<TInput> items,
+            Func<IParallelEnumerator<TInput>, IEnumerable<IAggregator<TOutput>>> func,
             Func<TInput, IList<TOutput>, TResult> resultSelector,
             int maxDegreeOfParallelism)
         {
             if (items is ICollection<TInput> col && col.Count == 0)
-                yield break; // return Array.Empty<TResult>();
+                yield break;
 
             var enumerator = new ParallelEnumerator<TInput>();
 
-            List<(TInput Input, Aggregator<TOutput>[] Agg)> perInputAggr = new();
+            // Нужно обязательно агрегировать funcs что-бы повторно итерировать после конвейера.
+            List<(TInput Input, IAggregator<TOutput>[] Agg)> itemsAgg = new();
 
             // Получим от пользователя все агрегаторы и инстанцируем замыкания.
             foreach (TInput input in items)
             {
                 enumerator.MoveNext(input);
                 var funcs = func(enumerator);
-                perInputAggr.Add((input, funcs.ToArray()));
+                itemsAgg.Add((input, funcs.ToArray()));
             }
 
-            var opt = new ExecutionDataflowBlockOptions
+            var actionBlock = new ActionBlock<IAggregator<TOutput>>(aggregator => aggregator.InvokeAsync(), new()
             {
                 EnsureOrdered = false,
                 SingleProducerConstrained = true,
                 BoundedCapacity = maxDegreeOfParallelism,
                 MaxDegreeOfParallelism = maxDegreeOfParallelism,
                 CancellationToken = CancellationToken.None
-            };
+            });
 
-            var actionBlock = new ActionBlock<Aggregator<TOutput>>(async aggregator =>
+            for (int i = 0; i < itemsAgg.Count; i++)
             {
-                aggregator.Result = await aggregator.Func().ConfigureAwait(false);
-            }, opt);
-
-            foreach (var input in perInputAggr)
-            {
-                foreach (var aggregator in input.Agg)
+                for (int j = 0; j < itemsAgg[i].Agg.Length; j++)
                 {
-                    if (!await actionBlock.SendAsync(aggregator).ConfigureAwait(false))
+                    if (!await actionBlock.SendAsync(itemsAgg[i].Agg[j]).ConfigureAwait(false))
                     // Произошла ошибка внутри конвейера.
                     {
                         // Извлекаем исключение.
@@ -115,23 +165,29 @@
             actionBlock.Complete();
             await actionBlock.Completion.ConfigureAwait(false);
 
-            foreach (var child in perInputAggr)
+            for (int i = 0; i < itemsAgg.Count; i++)
             {
-                TOutput[] outputArr = child.Agg.Select(x => x.Result).ToArray();
-                TResult final = resultSelector(child.Input, outputArr);
-                yield return final;
+                (TInput input, IAggregator<TOutput>[] agg) = itemsAgg[i];
+
+                List<TOutput> outputs = new(agg.Length);
+
+                for (int j = 0; j < agg.Length; j++)
+                {
+                    outputs.Add(agg[j].Result);
+                }
+                yield return resultSelector(input, outputs);
             }
         }
 
         public static async Task<IList<TResult>> Run<TInput, TOutput, TResult>(IEnumerable<TInput> items,
             Func<TInput, Task<TOutput>> func,
             Func<TInput, TOutput, TResult> selector,
-            int maxDegreeOfParallelism,
+            int maxDegreeOfParallelism = -1,
             CancellationToken cancellationToken = default)
         {
             return (await Run(items, func, maxDegreeOfParallelism, cancellationToken))
                 .Select(x => selector(x.Input, x.Output))
-                .ToArray();
+                .ToList();
         }
 
         public static async Task<IList<(TInput Input, TOutput Output)>> Run<TInput, TOutput>(IEnumerable<TInput> items,
@@ -141,20 +197,19 @@
             if (items is ICollection<TInput> col && col.Count == 0)
                 return Array.Empty<(TInput Input, TOutput Output)>();
 
-            var opt = new ExecutionDataflowBlockOptions
+            var transformBlock = new TransformBlock<TInput, (TInput, TOutput)>(async input =>
+            {
+                TOutput output = await func(input).ConfigureAwait(false);
+                return (input, output);
+            }, 
+            new ExecutionDataflowBlockOptions
             {
                 EnsureOrdered = false,
                 SingleProducerConstrained = true,
                 BoundedCapacity = maxDegreeOfParallelism,
                 MaxDegreeOfParallelism = maxDegreeOfParallelism,
                 CancellationToken = cancellationToken
-            };
-
-            var transformBlock = new TransformBlock<TInput, (TInput, TOutput)>(async input =>
-            {
-                var output = await func(input).ConfigureAwait(false);
-                return (input, output);
-            }, opt);
+            });
 
             var resultBuffer = new BufferBlock<(TInput Input, TOutput Output)>();
 
@@ -179,7 +234,6 @@
             }
             else
             {
-                //Debug.Assert(false);
                 return Array.Empty<(TInput Input, TOutput Output)>();
             }
         }
@@ -189,16 +243,14 @@
             Func<TInput, Task> func, int maxDegreeOfParallelism = DataflowBlockOptions.Unbounded,
             CancellationToken cancellationToken = default)
         {
-            var opt = new ExecutionDataflowBlockOptions
+            var actionBlock = new ActionBlock<TInput>(input => func(input), new()
             {
                 EnsureOrdered = false,
                 SingleProducerConstrained = true,
                 BoundedCapacity = maxDegreeOfParallelism,
                 MaxDegreeOfParallelism = maxDegreeOfParallelism,
                 CancellationToken = cancellationToken
-            };
-
-            var actionBlock = new ActionBlock<TInput>(input => func(input), opt);
+            });
 
             foreach (var item in items)
             {
@@ -211,6 +263,13 @@
             }
             actionBlock.Complete();
             await actionBlock.Completion.ConfigureAwait(false);
+        }
+
+        private static Aggregator<TOutput, TState> CreateAggregator<TOutput, TState>(
+            Func<TState, Task<TOutput>> func, 
+            TState state)
+        {
+            return new Aggregator<TOutput, TState>(func, state);
         }
     }
 }
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
@@ -8,45 +11,216 @@ namespace TestApp
 {
     class Program
     {
+        [DebuggerDisplay("PropertyId = {PropertyId}, {PropertyName}")]
+        class PropertyDto
+        {
+            public int PropertyId { get; set; }
+            public string? PropertyName { get; set; }
+        }
+
+        [DebuggerDisplay("OrderId = {OrderId}, {OrderName}")]
+        class OrderDto
+        {
+            public long OrderId { get; set; }
+            public string? OrderName { get; set; }
+        }
+
+        [DebuggerDisplay("ProductId = {ProductId}, {ProductName}")]
+        class ProductDto
+        {
+            public int ProductId { get; set; }
+            public string? ProductName { get; set; }
+        }
+
+        /// <summary>
+        /// Пользователь + Заказы.
+        /// </summary>
+        [DebuggerDisplay("Orders = {Orders.Count}")]
+        record User1(int UserId, IList<OrderDto> Orders);
+
+        /// <summary>
+        /// Пользователь + Заказы + Товары.
+        /// </summary>
+        [DebuggerDisplay("Orders = {Orders.Count}")]
+        record User2(int UserId, IList<Order1> Orders);
+
+        /// <summary>
+        /// Пользователь + Заказы + Товары + Свойства.
+        /// </summary>
+        [DebuggerDisplay("UserId = {UserId}, Orders = {Orders.Count}")]
+        record User3(int UserId, IList<Order2> Orders);
+
+        /// <summary>
+        /// Пользователь + Заказы + Товары + Свойства + Значения_Свойств.
+        /// </summary>
+        [DebuggerDisplay("Orders = {Orders.Count}")]
+        record User4(int UserId, IList<Order3> Orders);
+
+        /// <summary>
+        /// Заказ + Товары.
+        /// </summary>
+        [DebuggerDisplay("Products = {Products.Count}")]
+        record Order1(OrderDto Dto, IList<ProductDto> Products);
+
+        /// <summary>
+        /// Заказ + Товары + Свойства.
+        /// </summary>
+        [DebuggerDisplay("Products = {Products.Count}")]
+        record Order2(OrderDto Dto, IList<Product1> Products);
+
+        /// <summary>
+        /// Заказ + Товары + Свойства + Значения_Свойств.
+        /// </summary>
+        [DebuggerDisplay("Products = {Products.Count}")]
+        record Order3(OrderDto Dto, IList<Product2> Products);
+
+        /// <summary>
+        /// Товар + Свойства.
+        /// </summary>
+        [DebuggerDisplay("Properties = {Properties.Count}")]
+        record Product1(ProductDto Dto, IList<PropertyDto> Properties);
+
+        /// <summary>
+        /// Товар + Свойства + Значения_Свойств.
+        /// </summary>
+        [DebuggerDisplay("ProductName = {Dto.ProductName}, Properties = {Properties.Count}")]
+        record Product2(ProductDto Dto, IList<Property1> Properties);
+
+        /// <summary>
+        /// Свойство + Значение.
+        /// </summary>
+        [DebuggerDisplay("{Dto.PropertyName}, Value = {Value}")]
+        record Property1(PropertyDto Dto, string Value);
+
         static async Task Main()
         {
-            int[] itemIds = new[] { 1, 2, 3 };
+            int[] userIds = new[] { 1, 2, 3 };
 
-            // Для каждого Id получим коллекцию дочерних записей.
-            var items = await ParallelTransform.Run(itemIds, id => GetChilds(id),
-                (Id, Childs) => (Id, Childs),
-                maxDegreeOfParallelism: 10);
+            // Для каждого пользователя получим коллекцию заказов.
+            var users = await ParallelTransform.Run(userIds, GetOrders,
+                (UserId, Orders) => new User1(UserId, Orders));
 
-            foreach (var item in items)
+            foreach (User1 user in users)
             {
-                Console.WriteLine(item.Id);
-                foreach (long childId in item.Childs)
+                Debug.WriteLine(user.UserId);
+
+                foreach (OrderDto order in user.Orders)
                 {
-                    Console.WriteLine(childId);
+                    Debug.WriteLine(order.OrderId);
+                    Debug.WriteLine(order.OrderName);
                 }
             }
 
-            // Обогощаем дочерние записи "именем".
-            var withName = await ParallelTransform.Run(items, x =>
+            // Обогощаем заказы товарами.
+            var users2 = await ParallelTransform.Transform(users, x =>
             {
-                return x.Run(x.Item.Childs, childId => GetNameByChildId(childId), (Id, Name) => (Id, Name));
+                return x.Run(x.Item.Orders, o => GetOrderProducts(o.OrderId), (Order, Products) => new Order1(Order, Products));
             },
-                (item, Childs) => (item.Id, Childs),
-                maxDegreeOfParallelism: 10);
+            (User, Orders) => new User2(User.UserId, Orders), 1);
 
-            foreach (var item in withName)
+            foreach (User2 user in users2)
             {
-                Console.WriteLine(item.Id);
-                foreach (var child in item.Childs)
+                Debug.WriteLine(user.UserId);
+
+                foreach (Order1 order in user.Orders)
                 {
-                    Console.WriteLine(child.Id);
-                    Console.WriteLine(child.Name);
+                    Debug.WriteLine(order.Dto.OrderId);
+                    Debug.WriteLine(order.Dto.OrderName);
+
+                    foreach (ProductDto p in order.Products)
+                    {
+                        Debug.WriteLine(p.ProductId);
+                        Debug.WriteLine(p.ProductName);
+                    }
+                }
+            }
+
+            // Обогощаем товары характеристиками.
+            var users3 = await ParallelTransform.Transform(users2, x =>
+            {
+                return x.Sub(x.Item.Orders, xx =>
+                {
+                    return xx.Run(xx.Item.Products, p => GetProductProperties(p.ProductId), (Product, Properties) => new Product1(Product, Properties));
+                },
+                (Order, Products) => new Order2(Order.Dto, Products));
+            },
+            (User, Orders) => new User3(User.UserId, Orders), 1);
+
+
+            foreach (User3 user in users3)
+            {
+                Debug.WriteLine(user.UserId);
+
+                foreach (Order2 order in user.Orders)
+                {
+                    Debug.WriteLine(order.Dto.OrderId);
+                    Debug.WriteLine(order.Dto.OrderName);
+
+                    foreach (Product1 product in order.Products)
+                    {
+                        Debug.WriteLine(product.Dto.ProductId);
+                        Debug.WriteLine(product.Dto.ProductName);
+
+                        foreach (PropertyDto property in product.Properties)
+                            Debug.WriteLine(property.PropertyName);
+                    }
+                }
+            }
+
+            // Обогощаем характеристики товаров значениями.
+            var users4 = await ParallelTransform.Transform(users3, x =>
+            {
+                return x.Sub(x.Item.Orders, xx =>
+                {
+                    return x.Sub(xx.Item.Products, xxx =>
+                    {
+                        return xxx.Run(xxx.Item.Properties, pr => GetPropertyValue(pr.PropertyId), (Property, PValue) => new Property1(Property, PValue));
+                    },
+                    (Product, Properties) => new Product2(Product.Dto, Properties));
+                },
+                (Order, Products) => new Order3(Order.Dto, Products));
+            },
+            (User, Orders) => new User4(User.UserId, Orders), 1);
+
+
+            foreach (User4 user in users4)
+            {
+                Debug.WriteLine(user.UserId);
+
+                foreach (Order3 order in user.Orders)
+                {
+                    Debug.WriteLine(order.Dto.OrderId);
+                    Debug.WriteLine(order.Dto.OrderName);
+
+                    foreach (Product2 product in order.Products)
+                    {
+                        Debug.WriteLine(product.Dto.ProductId);
+                        Debug.WriteLine(product.Dto.ProductName);
+
+                        foreach (Property1 property in product.Properties)
+                        {
+                            Debug.WriteLine(property.Dto.PropertyName);
+                            Debug.WriteLine(property.Value);
+                        }
+                    }
                 }
             }
         }
 
-        static async Task<long[]> GetChilds(int id) => new long[] { 5, 6, 7 };
-
-        static async Task<string> GetNameByChildId(long childId) => $"child_{childId}";
+        static async Task<IList<OrderDto>> GetOrders(int userId) => new List<OrderDto> 
+        { 
+            new() { OrderId = 765, OrderName = "Заказ на 1 товар" } 
+        };
+        static async Task<IList<ProductDto>> GetOrderProducts(long orderId) => new List<ProductDto>
+        { 
+            new() { ProductId = 876, ProductName = "Молоток" },
+            new() { ProductId = 379, ProductName = "Пила" } 
+        };
+        static async Task<IList<PropertyDto>> GetProductProperties(int productId) => new List<PropertyDto>
+        { 
+            new() { PropertyId = 876, PropertyName = "Объём" },
+            new() { PropertyId = 987, PropertyName = "Вес" } 
+        };
+        static async Task<string> GetPropertyValue(int propertyId) => "2 кг";
     }
 }
